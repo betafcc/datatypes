aa83b37bd17cf39600f71ae85d060878e6755cb1
Beta Faccion <betafcc@gmail.com>
2018-08-21 01:01:58 -0300

    Adds basic prototype

--- datatypes/__init__.py
+++ datatypes/__init__.py
@@ -1,1 +1,24 @@
+from .util import make_class_dict
+
 __version__ = "0.1.0"
+
+
+def datatype(_cls=None, *, expose=False):
+    def wrap(cls):
+        metacls = type(cls)
+
+        for ctor_name, ctor_args in cls.__annotations__.items():
+            ctor = metacls(ctor_name, (cls,), make_class_dict(ctor_args))
+
+            setattr(cls, ctor_name, ctor)
+        return cls
+
+    if _cls is None:
+        return wrap
+    return wrap(_cls)
+
+
+def match(obj, cases):
+    for type, handler in cases.items():
+        if isinstance(obj, type):
+            return handler(*obj._bound_signature.args)
