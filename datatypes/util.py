6cd7dc3e5b2d27883861d3af0af3856a48f86c89
Beta Faccion <betafcc@gmail.com>
2018-08-22 02:48:36 -0300

    Adds util.parse_signature

--- datatypes/util.py
+++ datatypes/util.py
@@ -1,54 +1,14 @@
-from inspect import Signature, Parameter
+import inspect
 
 
-def init_method(self, *args, **kwargs):
-    signature = self.__signature__.bind(*args, **kwargs)
-    signature.apply_defaults()
+def parse_signature(
+    signature: str, closure: Dict[str, Any] = None
+) -> inspect.Signature:
+    locals().update(closure or {})
 
-    self._bound_signature = signature
+    if not signature.startswith("("):
+        signature = "(" + signature + ")"
 
+    exec(f"def dummy_function{signature}: pass")
 
-def repr_method(self):
-    return "{}({})".format(
-        self.__class__.__name__, ", ".join(map(str, self._bound_signature.args))
-    )
-
-
-def make_class_dict(constructor_arguments):
-    __signature__ = make_signature(*constructor_arguments)
-
-    return dict(
-        __init__=init_method,
-        __repr__=repr_method,
-        __signature__=__signature__,
-        __annotations__=extract_annotations(__signature__),
-    )
-
-
-def make_signature(*types):
-    return Signature(
-        [
-            Parameter(
-                name="_{}".format(n), kind=Parameter.POSITIONAL_ONLY, annotation=t
-            )
-            for n, t in enumerate(types)
-        ]
-    )
-
-
-def extract_annotations(signature):
-    return {k: v.annotation for k, v in signature.parameters.items()}
-
-
-# TODO
-def matches(a, b):
-    """
-    Returns True if `á` structually equals `b`
-
-    eg:
-    >>> matches(2, 3)
-    True
-    >>> matches([var('x')], [3])
-    True
-    """
-    return a == b
+    return inspect.signature(eval("dummy_function"))
