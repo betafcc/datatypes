0ffb7f2a3cafef5610b77414821dde185eecdcc3
Beta Faccion <betafcc@gmail.com>
2018-08-22 02:45:21 -0300

    Adds metadata makers

--- datatypes/class_.py
+++ datatypes/class_.py
@@ -15,12 +15,36 @@ def make_contructor(
 
 
 def make_init(signature: inspect.Signature) -> Callable[..., None]:
-    pass
+    def _init(self, *args, **kwargs):
+        bound = signature.bind(*args, **kwargs)
+        bound.apply_defaults()
+
+        self._bound_signature = bound
+
+    return _init
 
 
 def make_annotations(signature: inspect.Signature) -> Dict[str, Any]:
-    pass
+    return {k: v.annotation for k, v in signature.parameters.items()}
 
 
 def make_repr(signature: inspect.Signature) -> Callable[[Any], str]:
-    pass
+    if all(
+        parameter.kind is Parameter.POSITIONAL_ONLY
+        for parameter in signature.parameters.values()
+    ):
+
+        def _repr(self):
+            return self.__class__.__qualname__ + ", ".join(
+                map(repr, self._bound_signature.args)
+            )
+
+    else:
+
+        def _repr(self):
+            return (
+                self.__class__.__qualname__
+                + re.findall(r"^.+?(\(.+\)).+", repr(self._bound_signature))[0]
+            )
+
+    return _repr
